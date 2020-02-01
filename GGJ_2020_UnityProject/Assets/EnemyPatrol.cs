f80f32a7399461eaae55eda149ab7dd63a59915f
Remi Storms <ultimate.spidey.18@gmail.com>
2020-02-01 13:33:48 -0600

    Basic Patrol Enemy

---
+++ GGJ_2020_UnityProject/Assets/EnemyPatrol.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyPatrol : StateMachineBehaviour
+{
+    [SerializeField] private Transform[] patrolPoints;
+    private Transform currentPatrolTarget;
+    public float distanceFromCurrentTarget;
+    public int currentPatrolTargetIndex;
+    private NavMeshAgent navMeshAgent;
+    private Enemy enemy;
+
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        enemy = animator.GetComponent<Enemy>();
+        navMeshAgent = animator.GetComponent<NavMeshAgent>();
+        patrolPoints = enemy.GetPatrolPoints();
+
+        currentPatrolTarget = patrolPoints[0];
+        navMeshAgent.SetDestination(currentPatrolTarget.position);
+        navMeshAgent.isStopped = false;
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        Debug.Log("Patrol State Update : " + patrolPoints.Length + " patrol points found");
+
+        distanceFromCurrentTarget = Vector3.Distance( animator.transform.position, currentPatrolTarget.position);
+
+        if (distanceFromCurrentTarget <= 0.1f)
+        {
+            GetNextPosition();       
+        }
+
+    }
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+
+    }
+
+    private void GetNextPosition()
+    {
+        currentPatrolTargetIndex++;
+
+        if (currentPatrolTargetIndex >= patrolPoints.Length)
+        {
+            currentPatrolTargetIndex = 0;
+        }
+
+        currentPatrolTarget = patrolPoints[currentPatrolTargetIndex];
+
+        navMeshAgent.SetDestination(currentPatrolTarget.position);
+        navMeshAgent.isStopped = false;
+    }
+
+}
---
+++ GGJ_2020_UnityProject/Assets/Scripts/Enemy.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private Animator m_EnemyAnimator;
+    [SerializeField] private NavMeshAgent m_EnemyAgent;
+    [SerializeField] private Transform[] m_PatrolPoints;
+    [SerializeField] private float m_TimeToStayAlert;
+    [SerializeField] private float m_MoveSpeed;
+    [SerializeField] private float m_RunSpeed;
+
+    private Transform currentPatrolTarget;
+    private int currentPatrolTargetIndex = 0;
+
+    private void Awake()
+    {
+        ResetEnemy();
+    }
+
+    private void ResetEnemy()
+    {
+        m_EnemyAgent = GetComponent<NavMeshAgent>();
+        m_EnemyAnimator = GetComponent<Animator>();
+        m_EnemyAgent.isStopped = true;
+        
+    }
+
+    public Transform[] GetPatrolPoints()
+    {
+        return m_PatrolPoints;
+    }
+
+    public void GoToNextPosition()
+    {
+        StartCoroutine(_GoToNextPositionRoutine());
+    }
+
+    IEnumerator _GoToNextPositionRoutine()
+    {
+        yield return new WaitForSeconds(1.5f);
+    }
+}
